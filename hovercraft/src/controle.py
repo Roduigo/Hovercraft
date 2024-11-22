69204fb127155ec275cab9120739ec3a26c8c4e0
Rodrigo Kurosawa <rodrigokurosawa@usp.br>
2024-11-22 01:04:58 -0300

    Falta tunar o PID + filtros(caso necessário)

--- hovercraft/src/controle.py
+++ hovercraft/src/controle.py
@@ -24,7 +24,7 @@ class ContVelocidade:
         self.frente = 0
         self.referencia = 10000000 # distancia em quantidade de pixels para parar o robo
         self.min_pixels = 10000 #valor minínimo para o robo ainda identificar objeto
-        self.setpoint_giro = 0
+        self.setpoint_relativo = 0
         # parametros da camera
         self.freq_cam = 30
         self.altura = 480
@@ -36,7 +36,7 @@ class ContVelocidade:
         self.PID_giro = PID(1/600,0, 0, 1/self.freq_cam)
         self.PID_frente = PID(7/800000,0,0,1/self.freq_cam,0 ,100) 
         #valores entre 1/80000 e 1/800000
-        
+    
     def vel_control(self, data): #callback do yaw da px4
         
         self.vel = Float32MultiArray()#cria um objeto do tipo Twist para alterar a velocidade
@@ -55,10 +55,12 @@ class ContVelocidade:
     
         ########## GIRO ###########
     
-        yaw = convert_graus(data)
+        self.angulo_pix = convert_graus(data) #posição da pix em graus
+        
+        setpoint_giro = self.setpoint_relativo - self.angulo_pix
         
         # o PID vai retornar a direção em que o robo deverá girar
-        self.rot = self.PID_giro.compute(setpoint) # calcula o o setpoint em graus e joga no PID 
+        self.rot = self.PID_giro.compute(setpoint_giro) # calcula o o setpoint em graus e joga no PID 
         
         if(self.rot>0):
             self.vel.data[0] -= abs(self.rot)
@@ -70,7 +72,7 @@ class ContVelocidade:
         self.pub.publish(self.vel)
         rospy.loginfo(f"distancia ao centro da tela{setpoint_giro}")
         rospy.loginfo(f"Ajustando rotação: angular.z = dir {self.vel.data[0]} esq = {self.vel.data[1]}")
-        
+    
     def convert_graus(self, valor):
         orientation = valor.orientation
         quaternion = [orientation.x,orientation.y,orientation.z,orientation.w]
@@ -84,7 +86,8 @@ class ContVelocidade:
         self.dist = int(dist32.data)
         
     def angular(self,data):
-        self.setpoint_giro = data.x * self.k_graus #setpoint da camera dado em graus
+        setpoint_camera = data.x * self.k_graus #setpoint da camera dado em graus
+        self.setpoint_relativo =  self.angulo_pix + setpoint_camera # valor relativo da posição do objeto calculado a partir da pix
         
 if __name__ == '__main__':
     rospy.init_node('move_and_turn', anonymous=False) #Inicializa o nó do ROS
